e2a386fab83112bfe2a804e325f90bcc8565a301
Kame-Bashuna <basshunakamrroba@gmail.com>
2024-02-20 11:58:13 +0300

    Add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,70 @@
+fun main(){
+    var num1 =8
+    var num2 =5
+    var result = num1 % num2
+    println(result)
+
+     num1 = 64
+     num2 = 35
+     result = num1 * num2
+    println(result)
+
+    num1 = 64
+    num2 = 35
+    result = num1 + num2
+    println(result)
+
+    num1 = 64
+    num2 = 35
+    result = num1 - num2
+    println(result)
+
+    num1 = 64
+    num2 = 35
+    result = num1 / num2
+    println(result)
+
+    var a = 8
+    var b = 5
+    var c = a + b
+    println(c)
+
+     a = 8
+     b = 5
+     a += b
+    println(a)
+
+    a = 8
+    b = 5
+    a -= b
+    println(a)
+
+    a = 8
+    b = 5
+    a *= b
+    println(a)
+
+    a = 8
+    a++
+    println(a)
+
+    a = 8
+    a--
+    println(a)
+
+    a = 8
+    b = 5
+    println(a>b)
+
+    a = 8
+    b =5
+    println(a<=b)
+
+    a = 8
+    b = 5
+    println(a==b)
+
+    a = 5
+    b = 8
+    println(a!=b)
+}
